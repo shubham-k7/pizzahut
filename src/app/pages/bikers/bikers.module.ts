a39660f5c4eb174cde71cbd3716f1481112b5a9b
Shubham <shubham.khanna@prtouch.com>
2017-07-26 18:34:22 +0530

    Updated BikersOnMap

--- src/app/pages/bikers/bikers.module.ts
+++ src/app/pages/bikers/bikers.module.ts
@@ -6,17 +6,25 @@ import { AgmCoreModule } from '@agm/core';
 
 import { BikersComponent } from './bikers.component';
 import { BikerMapService } from './biker-map.service';
+import { NguiMapModule } from '@ngui/map';
+import { OrderOnMapService } from '../dashboard/order-on-map.service';
+
 import { MdSelectModule,MdInputModule,MdCardModule} from '@angular/material';
 @NgModule({
   imports: [
     CommonModule,
     routing,
     MdCardModule,
-    AgmCoreModule
+    AgmCoreModule,
+    NguiMapModule
   ],
   declarations: [
     BikersComponent
   ],
-  providers: [BikerMapService]
+  providers: [
+               BikerMapService,
+               OrderOnMapService
+               ]
 })
 export class BikersModule {}
+  
